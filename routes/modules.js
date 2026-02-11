305cdaeb472b4e36c5ac7d7af4d30e740388970e
Samilo69 <134307923+Samilo69@users.noreply.github.com>
2026-02-11 15:03:46 +0100

    Implement CRUD routes for modules with auth
    Added CRUD routes for module management with authentication.

--- routes/modules.js
+++ routes/modules.js
@@ -1,1 +1,18 @@
+const express = require("express");
+const router = express.Router();
+
+const moduleController = require("../controllers/moduleController");
+const authMiddleware = require("../middlewares/authMiddleware");
+
+// Toutes les routes modules sont protégées
+router.use(authMiddleware);
+
+// CRUD modules
+router.post("/", moduleController.createModule);
+router.get("/", moduleController.getAllModules);
+router.get("/:id", moduleController.getModuleById);
+router.put("/:id", moduleController.updateModule);
+router.delete("/:id", moduleController.deleteModule);
+
+module.exports = router;
 
